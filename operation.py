00f1ec134bff0b401be8140fd42e95be7d251a8a
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2017-03-14 12:45:05 -0500

    add coverage

--- operation.py
+++ operation.py
@@ -1,5 +1,5 @@
-def suma(a, b):
+def add(a, b):
     return a + b
 
-def resta(a, b):
+def substract(a, b):
     return a - b
--- operation_test.py
+++ operation_test.py
@@ -1,9 +1,9 @@
 import unittest
-from operation import suma, resta
+from operation import add, substract
 
 class OperationTest(unittest.TestCase):
-    def suma_test(self):
-        self.assertEqual(suma(1, 2), 3)
+    def add_test(self):
+        self.assertEqual(add(1, 2), 3)
 
-    # def resta_test(self):
-    #     self.assertEqual(resta(4, 1), 3)
+    # def substract_test(self):
+    #     self.assertEqual(substract(4, 1), 3)
