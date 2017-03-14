242ce75a04ed41a01fb64da500114075608c61b2
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2017-03-14 13:12:14 -0500

    Add test

--- README.rst
+++
@@ -1,5 +0,0 @@
-Coverage.py Badge
-==================
-
-.. image:: https://github.com/nanomag/test/blob/master/coverage.svg
-    :alt: 15%
--- operation_test.py
+++ operation_test.py
@@ -5,5 +5,5 @@ class OperationTest(unittest.TestCase):
     def add_test(self):
         self.assertEqual(add(1, 2), 3)
 
-    # def substract_test(self):
-    #     self.assertEqual(substract(4, 1), 3)
+    def substract_test(self):
+        self.assertEqual(substract(4, 1), 3)
